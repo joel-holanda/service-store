39d4937837745f819bb525a2df1b3174fce9c5b8
Kayquedias <kayquedias204@gmail.com>
2024-09-13 16:55:44 -0300

    refactor: Order page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sgnh.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media-cdn.tripadvisor.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
--- src/components/Orders/Order.tsx
+++ src/components/Orders/Order.tsx
@@ -2,9 +2,10 @@
 
 import React from 'react'
 import { useCardContext } from '@/contexts/useCardContext'
-import { Card } from '@chakra-ui/react'
+import { Card, Divider, Grid, GridItem, Heading } from '@chakra-ui/react'
 import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Order() {
   const { items } = useCardContext()
@@ -17,17 +18,27 @@ export default function Order() {
           <span className="text-lg">Voltar</span>
         </Link>
 
-        {items.map((it) => (
-          <Card key={it.id} className="w-full h-[100px] bg-orange-400">
-            <h1 className="text-center text-4xl border-b-2 border-b-sky-200">
-              {it.nameTitle}
-            </h1>
-            <div className="flex justify-between px-3 mt-4 text-1xl">
-              <p>{it.name}</p>
-              <p>R$ {it.price}</p>
-            </div>
-          </Card>
-        ))}
+        {items.map((it) => {
+          return (
+            <>
+              <Card key={it.id} className="w-full h-[100px]">
+                <Grid templateColumns="100px 1fr" gap={10}>
+                  <GridItem>
+                    <Image src={it.image} alt="" width={100} height={100} />
+                  </GridItem>
+                  <GridItem>
+                    <Heading as="h4">1x {it.nameTitle}</Heading>
+                    <div className="flex justify-between px-3 mt-4 text-1xl">
+                      <p>{it.name}</p>
+                      <p>R$ {it.price}</p>
+                    </div>
+                  </GridItem>
+                </Grid>
+              </Card>
+              <Divider className="h-px bg-zinc-400" />
+            </>
+          )
+        })}
       </div>
     </>
   )
