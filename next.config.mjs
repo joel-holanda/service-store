95e083d0eda75cce59cb5699d9d3ee271eca982f
Kayquedias <kayquedias204@gmail.com>
2024-09-13 23:15:55 -0300

    refactor: use ProductItem component instead of Flavor component

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'media-cdn.tripadvisor.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'novonegocio.com.br',
+      },
     ],
   },
 }
--- src/containers/home-page/products-section/product/index.tsx
+++ src/containers/home-page/products-section/product/index.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 import { Product, ProductsSection } from '@/types/IFood'
-import ListFlavor from '@/components/ListFood/Flavors'
+import { Box, Text } from '@chakra-ui/react'
+import { useCardContext } from '@/contexts/useCardContext'
+import Image from 'next/image'
 
 type ProductProps = {
   product: Product
@@ -12,21 +14,54 @@ type ProductProps = {
 }
 
 export function ProductItem({ product, section }: ProductProps) {
+  const { items, setItems, quantity, setQuantity } = useCardContext()
+
+  const addItem = (product: Product) => {
+    setItems([...items, product])
+    setQuantity(quantity + 1)
+  }
+
   return (
     <motion.div
       className="cursor-pointer"
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
     >
-      <ListFlavor
-        id={product.id}
-        price={product.price}
-        description={product.description}
-        image={product.image}
-        name={product.name}
-        idTitle={section.id}
-        nameTitle={section.title}
-      />
+      <Box
+        p="5"
+        maxW="400px"
+        borderWidth="2px"
+        borderRadius="lg"
+        key={product.id}
+        onClick={() =>
+          addItem({ ...product, idTitle: section.id, nameTitle: section.title })
+        }
+      >
+        <div>
+          <Image
+            className="w-full"
+            src={product.image}
+            alt={product.description}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div>
+          <Text
+            className="text-dark/80"
+            mt={2}
+            fontSize="xl"
+            textAlign={'center'}
+            lineHeight="short"
+          >
+            <strong className="uppercase">{product.name}</strong>
+          </Text>
+          <Text className="text-sm text-gray-700/75">
+            {product.description}
+          </Text>
+          <Text className="border-t-2 mt-2">R$ {product.price}</Text>
+        </div>
+      </Box>
     </motion.div>
   )
 }
